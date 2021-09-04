9c96c834c721ff8d1d9242f77d237e7834bed68a
binosaho <bino.saho@gmail.com>
2021-09-04 09:37:44 +0530

    intial commit

--- com/uniliver/source_data_loading.py
+++ com/uniliver/source_data_loading.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
 
 
 
-# spark-submit --packages "mysql:mysql-connector-java:8.0.15,com.springml:spark-sftp_2.11:1.1.1" com/uniliver/source_data_loading.py
+# spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4,mysql:mysql-connector-java:8.0.15,com.springml:spark-sftp_2.11:1.1.1,org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" com/uniliver/source_data_loading.py
 # spark-submit --packages "com.springml:spark-sftp_2.11:1.1.1" dataframe/ingestion/others/systems/sftp_df.py
 #spark-submit --packages "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" com/uniliver/source_data_loading.py
 #spark-submit --packages "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" com/uniliver/source_data_loading.py
