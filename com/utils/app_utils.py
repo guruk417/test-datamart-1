72c30c9027d4fef648840258e3c3c732655bcad8
Guru <guru417@gmail.com>
2021-09-07 09:48:57 -0500

    initial commit

---
+++ com/utils/app_utils.py
@@ -0,0 +1,32 @@
+def read_from_mysql(jdbc_params, spark):
+    return spark \
+        .read.format("jdbc") \
+        .option("driver", "com.mysql.cj.jdbc.Driver") \
+        .options(**jdbc_params) \
+        .load()
+
+
+def read_from_sftp(spark,file_loc,sftp_options):
+    return spark \
+        .read.format("com.springml.spark.sftp") \
+        .options(**sftp_options)\
+        .load(file_loc)
+
+
+def write_to_sftp():
+    ""
+
+def get_redshift_jdbc_url(redshift_config: dict):
+    host = redshift_config["redshift_conf"]["host"]
+    port = redshift_config["redshift_conf"]["port"]
+    database = redshift_config["redshift_conf"]["database"]
+    username = redshift_config["redshift_conf"]["username"]
+    password = redshift_config["redshift_conf"]["password"]
+    return "jdbc:redshift://{}:{}/{}?user={}&password={}".format(host, port, database, username, password)
+
+
+def get_mysql_jdbc_url(mysql_config: dict):
+    host = mysql_config["mysql_conf"]["hostname"]
+    port = mysql_config["mysql_conf"]["port"]
+    database = mysql_config["mysql_conf"]["database"]
+    return "jdbc:mysql://{}:{}/{}?autoReconnect=true&useSSL=false".format(host, port, database)
